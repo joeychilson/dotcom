95f103635650570285edf51d045ed47b317fe377
Joey Chilson <joeychilson@outlook.com>
2024-06-04 02:25:29 -0500

    remove mode watcher

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-	import Sun from 'lucide-svelte/icons/sun';
-	import Moon from 'lucide-svelte/icons/moon';
-	import { toggleMode } from 'mode-watcher';
-
 	const navItems = {
 		'/': {
 			name: 'joeychilson.com'
@@ -32,18 +28,6 @@
 					</a>
 				{/each}
 			</div>
-			<button
-				on:click={toggleMode}
-				class="relative flex items-center px-2 py-1 transition-all hover:text-neutral-800 dark:hover:text-neutral-200"
-			>
-				<Sun
-					class="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-				/>
-				<Moon
-					class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-				/>
-				<span class="sr-only">Toggle theme</span>
-			</button>
 		</nav>
 	</div>
 </aside>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,9 @@
 <script>
-	import { ModeWatcher } from 'mode-watcher';
 	import Header from '$lib/components/header.svelte';
 	import Footer from '$lib/components/footer.svelte';
 
 	import '../app.css';
 </script>
-
-<ModeWatcher />
 
 <main class="mx-4 mt-8 max-w-3xl antialiased lg:mx-auto">
 	<div class="mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0">
